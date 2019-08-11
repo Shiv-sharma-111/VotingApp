57864bab51cbe64d313846f59c7355f1aa300225
Shiv Sharma <shivsharmavictory@gmail.com>
2019-08-11 09:55:37 +0530

    Learning some java functionality

--- app/src/main/java/com/example/votingapp/MainActivity.java
+++ app/src/main/java/com/example/votingapp/MainActivity.java
@@ -5,24 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edt_name;
+    private Button btn_submit;
+    private TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tv = findViewById(R.id.tv_text);
-        Button submitButton = findViewById(R.id.btn_submit);
+        edt_name = findViewById(R.id.edt_name);
+        btn_submit = findViewById(R.id.btn_submit);
+        tv_result = findViewById(R.id.tv_result);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                tv.setText("Hey it worked");
-
+            public void onClick(View view) {
+                onSubmitted();
             }
         });
+
+    }
+    public void onSubmitted(){
+        String name = edt_name.getText().toString().trim();
+        if(!name.isEmpty()){
+            tv_result.setText(name);
+        }
     }
 }
